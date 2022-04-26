fb389b8f7318a52b5c4c8366d9e7372275bbe1a6
robertolatini31 <robertolatini31@gmail.com>
2022-04-26 16:12:01 +0200

    adjusted photo selection

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,7 @@ const app = new Vue ({
         contacts: [
             {
                 name: 'Michele',
-                avatar: './assets/img/avatar_1.jpg',
+                avatar: '_1',
                 visible: true,
                 messages: [
                     {
@@ -28,7 +28,7 @@ const app = new Vue ({
             },
             {
                 name: 'Fabio',
-                avatar: './assets/img/avatar_2.jpg',
+                avatar: '_2',
                 visible: true,
                 messages: [
                     {
@@ -50,7 +50,7 @@ const app = new Vue ({
             },
             {
                 name: 'Samuele',
-                avatar: './assets/img/avatar_3.jpg',
+                avatar: '_3',
                 visible: true,
                 messages: [
                     {
@@ -72,7 +72,7 @@ const app = new Vue ({
             },
             {
                 name: 'Alessandro B.',
-                avatar: './assets/img/avatar_4.jpg',
+                avatar: '_4',
                 visible: true,
                 messages: [
                     {
@@ -89,7 +89,7 @@ const app = new Vue ({
             },
             {
                 name: 'Alessandro L.',
-                avatar: './assets/img/avatar_5.jpg',
+                avatar: '_5',
                 visible: true,
                 messages: [
                     {
@@ -106,7 +106,7 @@ const app = new Vue ({
             },
             {
                 name: 'Claudia',
-                avatar: './assets/img/avatar_6.jpg',
+                avatar: '_6',
                 visible: true,
                 messages: [
                     {
@@ -128,7 +128,7 @@ const app = new Vue ({
             },
             {
                 name: 'Federico',
-                avatar: './assets/img/avatar_7.jpg',
+                avatar: '_7',
                 visible: true,
                 messages: [
                     {
@@ -145,7 +145,7 @@ const app = new Vue ({
             },
             {
                 name: 'Davide',
-                avatar: './assets/img/avatar_8.jpg',
+                avatar: '_8',
                 visible: true,
                 messages: [
                     {
@@ -168,6 +168,9 @@ const app = new Vue ({
         ]
     },
     methods: {
+        imgUrl(index) {
+            return `./assets/img/avatar${this.contacts[index].avatar}.jpg`
+        },
         activeMyChat(index) {
             this.activeChat = index;
         },
