a382fe0577e866f19109cf28750d8486089632f5
robertolatini31 <robertolatini31@gmail.com>
2022-04-27 18:10:49 +0200

    added banner to delete single message

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,16 +11,19 @@ const app = new Vue ({
                 visible: true,
                 messages: [
                     {
+                        bannerSingleMessage: false,
                         date: '10/01/2020 15:30:55',
                         message: 'Hai portato a spasso il cane?',
                         status: 'sent'
                     },
                     {
+                        bannerSingleMessage: false,
                         date: '10/01/2020 15:50:00',
                         message: 'Ricordati di stendere i panni',
                         status: 'sent'
                     },
                     {
+                        bannerSingleMessage: false,
                         date: '10/01/2020 16:15:22',
                         message: 'Tutto fatto!',
                         status: 'received'
@@ -33,16 +36,19 @@ const app = new Vue ({
                 visible: true,
                 messages: [
                     {
+                        bannerSingleMessage: false,
                         date: '20/03/2020 16:30:00',
                         message: 'Ciao come stai?',
                         status: 'sent'
                     },
                     {
+                        bannerSingleMessage: false,
                         date: '20/03/2020 16:30:55',
                         message: 'Bene grazie! Stasera ci vediamo?',
                         status: 'received'
                     },
                     {
+                        bannerSingleMessage: false,
                         date: '20/03/2020 16:35:00',
                         message: 'Mi piacerebbe ma devo andare a fare la spesa.',
                         status: 'sent'
@@ -55,16 +61,19 @@ const app = new Vue ({
                 visible: true,
                 messages: [
                     {
+                        bannerSingleMessage: false,
                         date: '28/03/2020 10:10:40',
                         message: 'La Marianna va in campagna',
                         status: 'received'
                     },
                     {
+                        bannerSingleMessage: false,
                         date: '28/03/2020 10:20:10',
                         message: 'Sicuro di non aver sbagliato chat?',
                         status: 'sent'
                     },
                     {
+                        bannerSingleMessage: false,
                         date: '28/03/2020 16:15:22',
                         message: 'Ah scusa!',
                         status: 'received'
@@ -77,11 +86,13 @@ const app = new Vue ({
                 visible: true,
                 messages: [
                     {
+                        bannerSingleMessage: false,
                         date: '10/01/2020 15:30:55',
                         message: 'Lo sai che ha aperto una nuova pizzeria?',
                         status: 'sent'
                     },
                     {
+                        bannerSingleMessage: false,
                         date: '10/01/2020 15:50:00',
                         message: 'Si, ma preferirei andare al cinema',
                         status: 'received'
@@ -94,11 +105,13 @@ const app = new Vue ({
                 visible: true,
                 messages: [
                     {
+                        bannerSingleMessage: false,
                         date: '10/01/2020 15:30:55',
                         message: 'Ricordati di chiamare la nonna',
                         status: 'sent'
                     },
                     {
+                        bannerSingleMessage: false,
                         date: '10/01/2020 15:50:00',
                         message: 'Va bene, stasera la sento',
                         status: 'received'
@@ -111,16 +124,19 @@ const app = new Vue ({
                 visible: true,
                 messages: [
                     {
+                        bannerSingleMessage: false,
                         date: '10/01/2020 15:30:55',
                         message: 'Ciao Claudia, hai novità?',
                         status: 'sent'
                     },
                     {
+                        bannerSingleMessage: false,
                         date: '10/01/2020 15:50:00',
                         message: 'Non ancora',
                         status: 'received'
                     },
                     {
+                        bannerSingleMessage: false,
                         date: '10/01/2020 15:51:00',
                         message: 'Nessuna nuova, buona nuova',
                         status: 'sent'
@@ -133,11 +149,13 @@ const app = new Vue ({
                 visible: true,
                 messages: [
                     {
+                        bannerSingleMessage: false,
                         date: '10/01/2020 15:30:55',
                         message: 'Fai gli auguri a Martina che è il suo compleanno!',
                         status: 'sent'
                     },
                     {
+                        bannerSingleMessage: false,
                         date: '10/01/2020 15:50:00',
                         message: 'Grazie per avermelo ricordato, le scrivo subito!',
                         status: 'received'
@@ -150,16 +168,19 @@ const app = new Vue ({
                 visible: true,
                 messages: [
                     {
+                        bannerSingleMessage: false,
                         date: '10/01/2020 15:30:55',
                         message: 'Ciao, andiamo a mangiare la pizza stasera?',
                         status: 'received'
                     },
                     {
+                        bannerSingleMessage: false,
                         date: '10/01/2020 15:50:00',
                         message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
                         status: 'sent'
                     },
                     {
+                        bannerSingleMessage: false,
                         date: '10/01/2020 15:51:00',
                         message: 'OK!!',
                         status: 'received'
@@ -178,6 +199,7 @@ const app = new Vue ({
             const newTime = new Date().toLocaleTimeString();
             // console.log(newDate, newTime);
             const objNewMessage = {
+                bannerSingleMessage: false,
                 date: newDate + ' ' + newTime, 
                 message: this.newMessage,
                 status: 'sent'
@@ -191,6 +213,7 @@ const app = new Vue ({
                 const newTime = new Date().toLocaleTimeString();
                 // console.log(newDate, newTime);
                 const objNewMessage = {
+                    bannerSingleMessage: false,
                     date: newDate + ' ' + newTime, 
                     message: 'OK',
                     status: 'received'
@@ -226,6 +249,18 @@ const app = new Vue ({
                     contact.visible = true;
                 }
             });
+        },
+        bannerSingle(index) {
+            if (this.contacts[this.activeChat].messages[index].bannerSingleMessage) {
+              console.log('era true metto false');
+              this.contacts[this.activeChat].messages[index].bannerSingleMessage = false;
+            } else {
+              console.log('era false metto true');
+              this.contacts[this.activeChat].messages[index].bannerSingleMessage = true;
+            }
+        },
+        deleteMessage(index) {
+            this.contacts[this.activeChat].messages[index].message = ''
         }
     }
 })
